90346a56d069f77513546e4e3287bf7ff74c4883
R.M.W.O.Ranasinghe <oshinikar@gmail.com>
2026-03-14 21:35:40 +0530

    Add comments for clarity in mapper function
    Added comments to clarify the purpose of the code and the checks performed on the CSV input.

--- src/mapper.py
+++ src/mapper.py
@@ -3,22 +3,26 @@ import sys
 import csv
 
 def mapper():
+    # In Hadoop Streaming, input is provided through stdin
     reader = csv.reader(sys.stdin)
     header_skipped = False
 
+    # Loop through each row in the CSV input
     for row in reader:
         if not header_skipped:
             header_skipped = True
             continue
 
         try:
+            #Check if the row has enough columns
+            #If it has fewer than 17 columns, skip the row
             if len(row) < 17:
                 continue
 
-            country   = row[1].strip()
-            commodity = row[7].strip()
-            year      = row[15].strip()
-            price_str = row[16].strip()
+            country   = row[1].strip()   # Country name
+            commodity = row[7].strip()   # Commodity name
+            year      = row[15].strip()  # Year
+            price_str = row[16].strip()  # Price as string
 
             if not country or not commodity or not year or not price_str:
                 continue
